beb1600a45cafbd7414572b1bf21c0f790997935
Liyanah <38380026+Liyanah@users.noreply.github.com>
2018-04-14 20:05:14 +0300

    Add files via upload

---
+++ Cilk+/input.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int s;
+    return 0; }
---
+++ Cilk+/output.c
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio.h>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+using namespace std;
+int main() {
+ int s;
+ return 0; }
